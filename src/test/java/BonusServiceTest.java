60bd3c361852a3adc0d422d26d747a556fae3eb0
Kseny <shavyrinaks@mail.ru>
2023-05-18 19:42:57 +0500

    Corectedеthree commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -44,8 +44,8 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
+        boolean registered = false;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -61,7 +61,7 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1_000_000;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
